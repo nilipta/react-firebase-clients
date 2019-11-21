0d42546e8ec1c16cb5ab64ba2d60a4cee2d8cf80
Nilipta <niliptasatapathy@gmail.com>
2019-11-21 23:17:09 +0530

    Added dashboard to display clients dynamically, with cSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import AppNavbar from './components/layout/AppNavbar';
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+import AppNavbar from './components/layout/AppNavbar';
+import DashBoard from './components/layout/DashBoard';
 
 class App extends Component{
   render() {
@@ -10,7 +12,9 @@ class App extends Component{
         <div className="App">
           <AppNavbar />
           <div className="container">
-            <h1>Hello</h1>
+            <Switch>
+              <Route exact path="/" component={DashBoard} />
+            </Switch>
           </div>
         </div>
       </Router>
